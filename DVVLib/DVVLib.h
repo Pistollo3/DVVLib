b9c979120e854235dacbe5ecc89129464dbe8e7a
Daniele Viscuso <d.viscuso@reply.it>
2021-11-02 10:02:17 +0100

    roba

---
+++ DVVLib/DVVLib.h
@@ -0,0 +1,18 @@
+//
+//  DVVLib.h
+//  DVVLib
+//
+//  Created by Daniele Viscuso on 02/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DVVLib.
+FOUNDATION_EXPORT double DVVLibVersionNumber;
+
+//! Project version string for DVVLib.
+FOUNDATION_EXPORT const unsigned char DVVLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DVVLib/PublicHeader.h>
+
+
---
+++ DVVLib/DVVLib.swift
@@ -0,0 +1,14 @@
+//
+//  DVVLib.swift
+//  DVVLib
+//
+//  Created by Daniele Viscuso on 02/11/21.
+//
+
+import Foundation
+
+public class DVVLib {
+    public static func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
