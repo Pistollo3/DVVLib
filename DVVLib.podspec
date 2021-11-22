bc7705a4741c7b4059a1c30cbd6ac04b28f13a42
Daniele Viscuso <d.viscuso@reply.it>
2021-11-22 16:49:34 +0100

    new cards

--- DVVLib.podspec
+++ DVVLib.podspec
@@ -7,7 +7,7 @@
 Pod::Spec.new do |spec|
 
   spec.name         = "DVVLib"
-  spec.version      = "0.5.1"
+  spec.version      = "0.5.2"
   spec.summary      = "A CocoaPods library written in Swift"
 
   spec.description  = <<-DESC
--- DVVLib/SubComponentCardView.swift
+++ DVVLib/SubComponentCardView.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import RxSwift
 
 class SubComponentCardView: UIView {
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    var disposeBag = DisposeBag()
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        label.text = "Super Wifi!"
+        GHSuperWifiManager.shared.subscribeToOnboarding()
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] val in
+                if val == .dashboard {
+                    self?.topView.isHidden = false
+                    self?.bottomView.isHidden = true
+                } else {
+                    self?.topView.isHidden = true
+                    self?.bottomView.isHidden = false
+                }
+            }).disposed(by: disposeBag)
     }
 
 }
